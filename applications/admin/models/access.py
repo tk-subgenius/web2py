f97804863307b38336e271fe6d4eed9f4c9f80bf
perfectom <tom@klopf.cz>
2019-11-24 14:28:06 +0000

    allowing for DEV.flag file, to override admin protections

--- applications/admin/models/access.py
+++ applications/admin/models/access.py
@@ -20,9 +20,11 @@ if request.env.web2py_runtime_gae:
 else:
     is_gae = False
 
+dev_flag_file = os.path.join(os.path.join(request.folder,"..","..","DEV.flag"))
+
 if request.is_https:
     session.secure()
-elif not request.is_local and not DEMO_MODE:
+elif not os.path.isfile(dev_flag_file)  and not request.is_local and not DEMO_MODE:
     raise HTTP(200, T('Admin is disabled because insecure channel'))
 
 try:
